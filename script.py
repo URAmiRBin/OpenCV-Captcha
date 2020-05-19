1288327c85c474a81ec078ddbed52a32968f22f5
unknown <ur.amirbin@gmail.com>
2020-05-19 13:02:12 +0430

    Added Tesseract OCR

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
 import cv2
 import numpy as np
 import sys
+import pytesseract
+from PIL import Image
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# SETTINGS
+KERNEL_SIZE = (5, 5)
+BLUR_INTENSITY = 0
+MIN_THRESH = 100
+MAX_THRESH = 200
 
 def image_read(image_address):
     image = cv2.imread(image_address)
@@ -23,17 +31,11 @@ def image_canny(image):
     return edged_image
 
 
-# SETTINGS
-KERNEL_SIZE = (5, 5)
-BLUR_INTENSITY = 0
-MIN_THRESH = 100
-MAX_THRESH = 200
-
 
 
 address = 'TestCases/' + sys.argv[1] + '.jpg'
 original = image_read(address)
-blurred = cv2.medianBlur(original, 11)
-edged = image_canny(blurred)
-cv2.imshow('Edged Image', edged)
-cv2.waitKey(0)
+gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+text = pytesseract.image_to_string(gray, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
+print(text)
